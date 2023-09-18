dc0a0a2b1d717f5c2409611662fe07c1ba7572b2
DiegoGarcez <dggokb@gmail.com>
2023-09-18 15:06:57 -0400

    criando SerializadorDeObjetoJson para encapsular a conversão

---
+++ src/main/java/br/com/diego/pscicologia/comum/SerializadorDeObjetoJson.java
@@ -0,0 +1,17 @@
+package br.com.diego.pscicologia.comum;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class SerializadorDeObjetoJson {
+
+    public static String serializar(final Object obj) {
+        try {
+            return new ObjectMapper()
+                    .registerModule(new JavaTimeModule())
+                    .writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
