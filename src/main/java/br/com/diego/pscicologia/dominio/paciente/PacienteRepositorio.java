cbb465b72d4ef0175921d2984e8ee72995e90ba8
DiegoGarcez <dggokb@gmail.com>
2023-09-19 15:57:46 -0400

    criando método no repositorio do paciente para obter somente ativos

--- src/main/java/br/com/diego/pscicologia/dominio/paciente/PacienteRepositorio.java
+++ src/main/java/br/com/diego/pscicologia/dominio/paciente/PacienteRepositorio.java
@@ -1,8 +1,13 @@
 package br.com.diego.pscicologia.dominio.paciente;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PacienteRepositorio extends MongoRepository<Paciente, String> {
+    @Query("{inativo :false}")
+    public List<Paciente> buscarAtivos();
 }
---
+++ src/test/java/br/com/diego/pscicologia/porta/adaptador/persistencia/paciente/PacienteRepositorioMongoDbTest.java
@@ -0,0 +1,40 @@
+package br.com.diego.pscicologia.porta.adaptador.persistencia.paciente;
+
+import br.com.diego.pscicologia.builder.PacienteBuilder;
+import br.com.diego.pscicologia.dominio.paciente.Paciente;
+import br.com.diego.pscicologia.dominio.paciente.PacienteRepositorio;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DataMongoTest
+public class PacienteRepositorioMongoDbTest {
+
+    @Autowired
+    private PacienteRepositorio pacienteRepositorio;
+
+    @BeforeEach
+    void setUp() {
+        pacienteRepositorio.deleteAll();
+    }
+
+    @Test
+    public void deveSerPossivelBuscarSomentePacientesAtivos() {
+        Paciente pacienteAtivo = new PacienteBuilder().ativo().criar();
+        Paciente outroPacienteAtivo = new PacienteBuilder().ativo().criar();
+        Paciente pacienteInativo = new PacienteBuilder().inativo().criar();
+        List<Paciente> pacientes = Arrays.asList(pacienteAtivo, outroPacienteAtivo, pacienteInativo);
+        pacienteRepositorio.saveAll(pacientes);
+
+        List<Paciente> pacientesObtidos = pacienteRepositorio.buscarAtivos();
+
+        Assertions.assertThat(pacientesObtidos).usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrder(pacienteAtivo, outroPacienteAtivo);
+    }
+}
+
