36f65289232ac38d0dbb2aac9e6948cff4c7311a
DiegoGarcez <dggokb@gmail.com>
2023-09-21 10:35:21 -0400

    alterando PacienteDTO com novo ValorDTO

---
+++ src/main/java/br/com/diego/pscicologia/servico/paciente/consulta/ValorDTO.java
@@ -0,0 +1,10 @@
+package br.com.diego.pscicologia.servico.paciente.consulta;
+
+import java.math.BigDecimal;
+
+public class ValorDTO {
+    public Integer quantidaDeDiasNoMes;
+    public BigDecimal valorPorSessao;
+    public String mes;
+    public Integer ano;
+}
