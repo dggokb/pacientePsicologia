1361280bb2f298e78f6928e33d02a1534d25e324
DiegoGarcez <dggokb@gmail.com>
2023-10-03 14:47:03 -0400

    adicionando no application configuração do mongodb

--- src/main/java/br/com/diego/pscicologia/PscicologiaApplication.java
+++ src/main/java/br/com/diego/pscicologia/PscicologiaApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class PscicologiaApplication {
 
 	@Bean
