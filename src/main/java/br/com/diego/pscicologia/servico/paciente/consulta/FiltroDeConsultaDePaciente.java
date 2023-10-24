e9267c103e7fb6a3602b111babae14803b0c9733
DiegoGarcez <dggokb@gmail.com>
2023-10-24 11:28:48 -0400

    adicionando nome no FiltroDeConsultaDePaciente

--- src/main/java/br/com/diego/pscicologia/servico/paciente/consulta/FiltroDeConsultaDePaciente.java
+++ src/main/java/br/com/diego/pscicologia/servico/paciente/consulta/FiltroDeConsultaDePaciente.java
@@ -5,13 +5,23 @@ import br.com.diego.pscicologia.comum.FiltroDeConsulta;
 public class FiltroDeConsultaDePaciente implements FiltroDeConsulta {
 
     private String id;
+    private String nome;
 
-    public FiltroDeConsultaDePaciente comId(String id){
+    public FiltroDeConsultaDePaciente comId(String id) {
         this.id = id;
+        return this;
+    }
+
+    public FiltroDeConsultaDePaciente comNome(String nome) {
+        this.nome = nome;
         return this;
     }
 
     public String getId() {
         return id;
+    }
+
+    public String getNome() {
+        return nome;
     }
 }
